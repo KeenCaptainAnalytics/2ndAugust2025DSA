53526885e73586fb884152f588c1adf749429eab
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-09-13 11:46:06 +0530

    Create MergingTechnique.java

---
+++ sorting/MergingTechnique.java
@@ -0,0 +1,57 @@
+public class MergingTechnique {
+
+    static int[] merge(int arr1[] , int arr2[]){
+
+        int temp[] = new int[arr1.length + arr2.length ];
+
+        int  i =0 ; // starting index of arr1;
+        int j =0 ;  // starting index of arr2;
+        int k = 0 ; //temp array starting index
+
+        while(i<arr1.length && j <arr2.length){
+            if(arr1[i]<arr2[j]){
+                temp[k] = arr1[i];
+                i++;
+                k++;
+            }
+            else{
+                temp[k] = arr2[j];
+                j++;
+                k++;
+            }
+        }
+
+        while(i<arr1.length ){
+            temp[k] = arr1[i];
+            k++;
+            i++;
+        }
+         while(j<arr2.length ){
+            temp[k] = arr2[j];
+            k++;
+            j++;
+        }
+
+        return temp;
+    }
+
+    
+    public static void main(String[] args) {
+        
+
+        int arr1[] ={1,4,7};
+        int arr2[] ={3,5,9};
+        // sorted arrays
+        int result[] = merge(arr1, arr2);
+
+        print(result);
+    }
+
+    static void print(int arr[]){
+        for(int i =0 ;i <arr.length ;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+
+
+}
