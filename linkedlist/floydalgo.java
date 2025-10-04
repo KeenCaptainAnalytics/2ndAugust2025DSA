248f4939b2f0d279dbf8587edb950b692b1be589
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-10-04 11:08:21 +0530

    Create floydalgo.java

---
+++ linkedlist/floydalgo.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // fast slow
+        ListNode fast = head;
+        ListNode slow = head;
+
+// jumping step 
+// null.next
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
